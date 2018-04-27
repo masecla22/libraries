524691b1942add38bd82e04ba2eac1344a17f08f
masecla22 <supermasecla22@yahoo.com>
2018-04-27 15:04:04 +0300

    Created mattmark.h
    It's a benchmarking library, much, much simpler than <chrono> which measures time in microseconds.

---
+++ mattmark.h
@@ -0,0 +1,6 @@
+#include <chrono>
+namespace matt{namespace benchmark{class clock{private:std::chrono::high_resolution_clock::time_point beginPoint;bool wasEnded=0;
+public:double durs=0;clock(void){beginPoint = std::chrono::high_resolution_clock::now();}
+void start(){beginPoint = std::chrono::high_resolution_clock::now();}
+void ends(){std::chrono::high_resolution_clock::time_point endPoint = std::chrono::high_resolution_clock::now();
+std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(endPoint- beginPoint);durs = time_span.count()*1000000;}};}}
